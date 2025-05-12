97fa628f84140285d7dd45bd2c61d55fe120db0d
MMMazaira <mmmazaira@gmail.com>
2025-05-12 15:39:46 +0200

    Afegida funcionalitat per mostrar contactes

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // script.js
 console.log("Agenda Web");
+// Crea una llista de contactes
+const contactes = ["Jose", "Saida", "Ruben"];
+
+// Crea l'element <ul> per a la llista
+const llista = document.createElement('ul');
+
+// Per cada contacte, afegeix un element <li> a la llista
+contactes.forEach(nom => {
+  const li = document.createElement('li');
+  li.textContent = nom;
+  llista.appendChild(li);
+});
+
+// Afegeix la llista a la pàgina
+document.body.appendChild(llista);
